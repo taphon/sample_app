7313080e5a787ab64debd040ae276afebb846d59
taphon <taphon@gmail.com>
2011-10-31 13:12:07 -0400

    Basic user authentication complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,42 @@ class User < ActiveRecord::Base
   validates :password, :presence => true, 
                        :confirmation => true,
                        :length => { :within => 6..40 }
+                      
+  
+  before_save :encrypt_password
+  
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+
+  # Can also be called using Class << self
+  # Then define method as normal.
+  def User.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil  if user.nil?
+    return user if user.has_password?(submitted_password) 
+  end
+
+  private
+  
+  def encrypt_password
+    self.salt = make_salt if self.new_record?
+    self.encrypted_password = encrypt(self.password)
+  end
+
+  def encrypt(string)
+    secure_hash("#{salt}--#{string}")
+  end
+
+  def secure_hash(string)
+    Digest::SHA2.hexdigest(string)
+  end
+
+  def make_salt
+    secure_hash("#{Time.now.utc}--{password}")
+  end
 end
+
 
 # == Schema Information
 #
@@ -25,5 +60,6 @@ end
 #  created_at         :datetime
 #  updated_at         :datetime
 #  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
